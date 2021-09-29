2ba0212edb9305b1b4045ecf629c194e2878fee1
kyoya <kyoyaprog@gmail.com>
2021-09-29 20:34:08 +0900

    ABC131B問題

---
+++ test.py
@@ -0,0 +1,14 @@
+# ABC 131 B問題
+
+def test(n,l):
+    n = list(range(l, l+n))
+
+    if min(n) >= 0:
+        n.remove(min(n))
+    elif min(n) < 0 < max(n):
+         n.remove(0)
+    else:
+        n.remove(max(n))
+    print(sum(n))
+
+test(30,-50)
