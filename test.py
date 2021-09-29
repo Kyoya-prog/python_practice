4fa29c0036f22eb8d6d3cc49680837e7f7cefcad
kyoya <kyoyaprog@gmail.com>
2021-09-29 22:11:04 +0900

    ABC132B問題を実装

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # ABC131B問題
-def test(n,l):
+def test131B(n,l):
     n = list(range(l, l+n))
 
     if min(n) >= 0:
@@ -12,4 +12,19 @@ def test(n,l):
         n.remove(max(n))
     print(sum(n))
 
-test(30,-50)
+test131B(30,-50)
+
+def test132B(n,p):
+    result = 0
+    for i in range(0,n - 2):
+        q = [p[i],p[i + 1],p[i + 2]]
+        sorted_q = sorted(q)
+        if sorted_q[1] == q[1]:
+            result += 1
+        
+    
+    print(result)
+
+
+test132B(9,[9,6,3,2,5,8,7,4,1])
+
